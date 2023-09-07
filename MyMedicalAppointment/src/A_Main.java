bc641f5b80987e881b5ce1229bd012cb129c95f4
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2023-09-07 15:54:29 -0500

    my approach about java POO class 9: Variabls Constantes

---
+++ MyMedicalAppointment/src/A_Main.java
@@ -0,0 +1,7 @@
+import static ui.A_UIMenuConst.*;
+
+public class A_Main {
+    public static void secondMain(String[] args){
+        showMenu();
+    }
+}
---
+++ MyMedicalAppointment/src/Main.java
@@ -0,0 +1,23 @@
+//import static ui.UIMenu.*;
+import static ui.A_UIMenuConst.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Doctor myDoctor = new Doctor();
+        myDoctor.name = "Alejandro Rodríguez";
+        myDoctor.showName();
+        myDoctor.showId();
+
+
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();*/
+
+        showMenu();
+
+    }
+
+
+
+}
