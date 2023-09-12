1161e3fa9a3ec47520f91dfc9dc94a8c2747d812
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2023-09-12 00:24:47 -0500

    Adding patient Class and another files

---
+++ MyMedicalAppointment/src/Patient.java
@@ -0,0 +1,10 @@
+public class Patient {
+
+    String name, email, adress, phoneNumber, birthday, blood;
+    double weight, height;
+
+    Patient (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
