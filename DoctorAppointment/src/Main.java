d2c209f745e7bcc66c124bc65a82b715f845adee
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2024-01-15 00:32:10 -0500

    finished the course

--- DoctorAppointment/src/Main.java
+++ DoctorAppointment/src/Main.java
@@ -1,6 +1,46 @@
+import model.*;
+
+import java.util.Date;
+
+import static ui.UIMenu.showMenu;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        showMenu();
+
+
+
+/*
+        User user = new Doctor("Anahi", "an@na.co");
+        user.showData();
+        User userPa = new Patient("Anahi", "an@na.co");
+        userPa.showData(); */
+    /*    User user1 = new User("Anahi", "an@na.co") {
+            @Override
+            public void showData() {
+                System.out.println("Doctor\n");
+                System.out.println("Hospital: Cruz verde");
+                System.out.println("Departamento: Geriatría");
+            }
+        };
+        user1.showData();
+
+        ISchedulable mySchedule = new ISchedulable() {
+            @Override
+            public void schedule(Date date, String time) {
+
+            }
+        };
+*/
+
+/*
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        System.out.println(patient);*/
+
+
     }
+
 }
